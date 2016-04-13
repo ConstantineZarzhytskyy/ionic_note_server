de87897f5afe4c886e8a6b2b49edd2547bcf71f0
ConstantineZ <constantinez@interlink-ua.com>
2016-04-13 14:04:22 -0700

    Add PUT

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 
   next();
