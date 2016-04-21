30a801a21119e14e906c120c09bfcfaabf368959
ConstantineZ <constantinez@interlink-ua.com>
2016-04-21 17:56:30 +0300

    remove if in userinfo

--- app/route/user/user.js
+++ app/route/user/user.js
@@ -6,7 +6,6 @@ var User = configDb.User;
 router.route('/info')
     .get(function (req, res) {
       var user = req.user;
-      if (user.isLogged) { return res.end(); }
 
       User.findOne({ userId: user._id }, function (err, userDB) {
         if (err) { return res.send(err); }
